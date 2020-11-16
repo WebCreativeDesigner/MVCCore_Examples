0be63cf2cd07ee45229ab62a40621454eff5f86a
WebCreativeDesigner <nigeloldham@yahoo.com>
2020-11-16 13:46:16 +0000

    Updated file such as zip file in general folder.

--- Controllers/NthFibonacciController.cs
+++ Controllers/NthFibonacciController.cs
@@ -35,7 +35,16 @@ namespace MVCCore_Examples.Controllers
         public IActionResult Index(string Number)
         {
             //return View();
-            NthFibonacciModelList = (List<NthFibonacciModel>)repository.Get_Nth_Fibonacci_Repository(Convert.ToInt32(Number));
+            try
+            {
+                NthFibonacciModelList = (List<NthFibonacciModel>)repository.Get_Nth_Fibonacci_Repository(Convert.ToDouble(Number));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+                ModelState.AddModelError("", "The Number entered is the wrong type.  It should be an integer or a double.");
+            }
+
             return View(NthFibonacciModelList.ToList());
         }
 
